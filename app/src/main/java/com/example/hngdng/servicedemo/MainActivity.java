14f278326a61012af7c7a92166be52075ff02250
HngDng <vidang68@gmail.com>
2019-01-08 08:26:09 +0700

    them cac thu linh tinh

--- app/src/main/java/com/example/hngdng/servicedemo/MainActivity.java
+++ app/src/main/java/com/example/hngdng/servicedemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hngdng.servicedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +13,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         btnOpen = (Button) findViewById(R.id.btnOpen);
         btnStop = (Button) findViewById(R.id.btnStop);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nono(View v);
+            }
 
+            private void nono(View view) {
+            int id = view.getId();
+            if (id == R.id.btnOpen) {
+
+            }
+            else if (id == R.id.btnStop) {
+
+            }
+            }
+        });
     }
 }
